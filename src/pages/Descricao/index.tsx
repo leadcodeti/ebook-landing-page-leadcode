919718ccd4ba0b01502ee6ad6333578638a1e8fa
leonardocosta <sir.costa@yahoo.com.br>
2022-05-15 15:17:00 -0300

    Seprando conteúdo da página em constantes e substituindo syles.css por styles.module.scss

--- src/pages/Descricao/index.tsx
+++ src/pages/Descricao/index.tsx
@@ -1,56 +1,58 @@
-import "./styles.css";
+import styles from "./styles.module.scss";
 
 export function Descricao() {
+  const pageData = {
+    title: "Um guia para a sua landing page de alta conversão",
+    categories: [
+      {
+        icon: {
+          path: "../../images/cute-rocket-launching-1.png",
+          altText: "rocket",
+        },
+        title: "Descubra estratégias que funcionam.",
+        content:
+          "Aprenda estratégias eficazes que aumentarão o potencial de conversão de sua landing page.",
+      },
+      {
+        icon: { path: "../../images/file-1.png", altText: "file" },
+        title: "As 3 seções indispensáveis.",
+        content:
+          "Não deixe essas seções de fora de sua landing page ou você pode perder conversões.",
+      },
+      {
+        icon: { path: "../../images/cross-1.png", altText: "error" },
+        title: "Erros que você precisa evitar.",
+        content:
+          "Descubra o que evitar para não correr o risco de perder, além de conversões, a credibilidade.",
+      },
+      {
+        icon: { path: "../../images/view-1.png", altText: "eye" },
+        title: "Exemplos de landing pages que convertem",
+        content:
+          "Agora que você já sabe o que deve e o que não deve fazer, veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.",
+      },
+    ],
+  };
+
   return (
-    <div className="container">
-      <div className="titulo">
-        Um guia para a sua landing page de alta conversão
+    <div className={styles.wrapper}>
+      <h2>{pageData.title}</h2>
+      <div className={styles.gridContainer}>
+        {pageData.categories.map((e) => (
+          <div id="categoryContainer">
+            <div className={styles.categoryHeader}>
+              <img
+                className={e.icon.altText !== "eye" ? styles.icon : styles.eye}
+                src={e.icon.path}
+                alt={e.icon.altText}
+              />
+              <span>{e.title}</span>
+            </div>
+            <p>{e.content}</p>
+          </div>
+        ))}
       </div>
-      <div className="gridLayout">
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img
-              className="icon"
-              src="../../images/cute-rocket-launching-1.png"
-              alt="rocket"
-            />
-            <span>Descubra estratégias que funcionam</span>
-          </div>
-          <p>
-            Aprenda estratégias eficazes que aumentarão o potencial de conversão de sua landing page.
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon" src="../../images/file-1.png" alt="file" />
-            <span className="categoryTitle">As 3 seções indispensáveis</span>
-          </div>
-          <p>
-            Não deixe essas seções de fora de sua landing page ou você pode perder conversões.
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon" src="../../images/cross-1.png" alt="error" />
-            <span className="categoryTitle">Erros que você precisa evitar</span>
-          </div>
-          <p>
-            Descubra o que evitar para não correr o risco de perder, além de conversões, credibilidade. 
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon eye" src="../../images/view-1.png" alt="eye" />
-            <span className="categoryTitle">
-              Exemplos de landing pages que convertem
-            </span>
-          </div>
-          <p>
-            Agora que você já sabe o que fazer e o que não fazer, veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.
-          </p>
-        </div>
-      </div>
-      <hr className="divisor" />
+      <hr />
     </div>
   );
 }
