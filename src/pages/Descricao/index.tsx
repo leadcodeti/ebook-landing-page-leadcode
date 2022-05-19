acb2966e24a24c295a902ea2ebabc6b6ac85c75e
leonardocosta <sir.costa@yahoo.com.br>
2022-05-19 14:49:20 -0300

    Alterações no layout - recomendação da DS 18.05.22.

--- src/pages/Descricao/index.tsx
+++ src/pages/Descricao/index.tsx
@@ -9,27 +9,27 @@ export function Descricao() {
           path: "../../images/cute-rocket-launching-1.png",
           altText: "rocket",
         },
-        title: "Descubra estratégias que funcionam.",
+        title: "Descubra estratégias que funcionam",
         content:
           "Aprenda estratégias eficazes que aumentarão o potencial de conversão de sua landing page.",
       },
       {
-        icon: { path: "../../images/file-1.png", altText: "file" },
-        title: "As 3 seções indispensáveis.",
+        icon: { path: "../../images/file-1.png", altText: "file with ticked simbol" },
+        title: "As 3 seções indispensáveis",
         content:
-          "Não deixe essas seções de fora de sua landing page ou você pode perder conversões.",
+          "Não deixe essas seções de fora de sua landing page ou você perderá conversões.",
       },
       {
-        icon: { path: "../../images/cross-1.png", altText: "error" },
-        title: "Erros que você precisa evitar.",
+        icon: { path: "../../images/cross-1.png", altText: "x inside a filled circle" },
+        title: "Erros que você precisa evitar",
         content:
-          "Descubra o que evitar para não correr o risco de perder, além de conversões, a credibilidade.",
+          "Descubra o que evitar para não correr o risco de perder cliques e credibilidade.",
       },
       {
         icon: { path: "../../images/view-1.png", altText: "eye" },
-        title: "Exemplos de landing pages que convertem",
+        title: "Landing pages que convertem",
         content:
-          "Agora que você já sabe o que deve e o que não deve fazer, veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.",
+          "Veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.",
       },
     ],
   };
@@ -39,7 +39,7 @@ export function Descricao() {
       <h2>{pageData.title}</h2>
       <div className={styles.gridContainer}>
         {pageData.categories.map((e) => (
-          <div id="categoryContainer">
+          <div className={styles.categoryContainer}>
             <div className={styles.categoryHeader}>
               <img
                 className={e.icon.altText !== "eye" ? styles.icon : styles.eye}
