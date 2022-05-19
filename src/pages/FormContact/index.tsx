38bb843cceed453b493ef57cd2572d718f559408
leonardocosta <sir.costa@yahoo.com.br>
2022-05-19 17:20:44 -0300

    Alterações no layout do formulário.

--- src/pages/FormContact/index.tsx
+++ src/pages/FormContact/index.tsx
@@ -4,11 +4,11 @@ import styles from './styles.module.scss'
 
 export function FormContact() {
     return (
-        <>
+        <div className={styles.wrapper}>
             <div className={styles.hr}></div>
-            <h1>Inscreva-se para receber nossos e-books</h1>
+            <h1>Baixe o e-book agora e arrase nas landing pages</h1>
 
-            <div className={styles.container}>
+            <div className={styles.contentContainer}>
                 <div className={styles.ebookImg}>
                     <img src={ebookImg} alt="ebook digital imagem" />
                 </div>
@@ -18,12 +18,13 @@ export function FormContact() {
                             <img src={logoLeadCode} alt="logo LeadCode" />
                             &lt;LeadCode /&gt;
                         </div>
-                        <input type="text" placeholder='Digite seu melhor e-mail' />
-                        <button type='button'>Receber e-book</button>
+                        <input type="text" placeholder='Digite o seu nome' />
+                        <input type="email" placeholder='Digite seu melhor e-mail' />
+                        <button type='button'>Receber e-book grátis</button>
                     </form>   
                 </div>
             
             </div>
-        </>
+        </div>
     )
 }
