3bf63d0763fdab438395c7c3a409d95cd14243ea
HenriqueVPS <jibas1997@hotmail.com>
2022-05-20 22:47:19 -0300

    alterando containers da aplicação

--- src/pages/Descricao/index.tsx
+++ src/pages/Descricao/index.tsx
@@ -1,56 +1,57 @@
-import "./styles.css";
+import styles from "./styles.module.scss";
 
 export function Descricao() {
+  const pageData = {
+    title: "Um guia para a sua landing page de alta conversão",
+    categories: [
+      {
+        icon: {
+          path: "../../images/cute-rocket-launching-1.png",
+          altText: "rocket",
+        },
+        title: "Descubra estratégias que funcionam",
+        content:
+          "Aprenda estratégias eficazes que aumentarão o potencial de conversão de sua landing page.",
+      },
+      {
+        icon: { path: "../../images/file-1.png", altText: "file with ticked simbol" },
+        title: "As 3 seções indispensáveis",
+        content:
+          "Não deixe essas seções de fora de sua landing page ou você perderá conversões.",
+      },
+      {
+        icon: { path: "../../images/cross-1.png", altText: "x inside a filled circle" },
+        title: "Erros que você precisa evitar",
+        content:
+          "Descubra o que evitar para não correr o risco de perder cliques e credibilidade.",
+      },
+      {
+        icon: { path: "../../images/view-1.png", altText: "eye" },
+        title: "Landing pages que convertem",
+        content:
+          "Veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.",
+      },
+    ],
+  };
+
   return (
-    <div className="container">
-      <h1 className="titulo">
-        Um guia para a sua landing page de alta conversão
-      </h1>
-      <div className="gridLayout">
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img
-              className="icon"
-              src="../../images/cute-rocket-launching-1.png"
-              alt="rocket"
-            />
-            <span>Descubra estratégias que funcionam</span>
+    <div className={styles.wrapper}>
+      <h2>{pageData.title}</h2>
+      <div className={styles.gridContainer}>
+        {pageData.categories.map((e) => (
+          <div className={styles.categoryContainer}>
+            <div className={styles.categoryHeader}>
+              <img
+                className={e.icon.altText !== "eye" ? styles.icon : styles.eye}
+                src={e.icon.path}
+                alt={e.icon.altText}
+              />
+              <span>{e.title}</span>
+            </div>
+            <p>{e.content}</p>
           </div>
-          <p>
-            Aprenda estratégias eficazes que aumentarão o potencial de conversão de sua landing page.
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon" src="../../images/file-1.png" alt="file" />
-            <span className="categoryTitle">As 3 seções indispensáveis</span>
-          </div>
-          <p>
-            Não deixe essas seções de fora de sua landing page ou você pode perder conversões.
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon" src="../../images/cross-1.png" alt="error" />
-            <span className="categoryTitle">Erros que você precisa evitar</span>
-          </div>
-          <p>
-            Descubra o que evitar para não correr o risco de perder, além de conversões, credibilidade. 
-          </p>
-        </div>
-        <div className="categoryContainer">
-          <div className="categoryHeader">
-            <img className="icon eye" src="../../images/view-1.png" alt="eye" />
-            <span className="categoryTitle">
-              Exemplos de landing pages que convertem
-            </span>
-          </div>
-          <p>
-            Agora que você já sabe o que fazer e o que não fazer, veja exemplos para lhe inspirar a criar a sua landing page de alta conversão.
-          </p>
-        </div>
+        ))}
       </div>
-      <hr className="divisor" />
     </div>
   );
 }
