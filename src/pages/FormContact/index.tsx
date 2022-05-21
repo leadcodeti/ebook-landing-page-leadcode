6edaf4941c553f99b04527ed2a58bc85276b51a2
leonardocosta <sir.costa@yahoo.com.br>
2022-05-20 21:42:27 -0300

    Tentando enviar styles.module.scss de descrição.

---
+++ src/pages/FormContact/index.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
+import ebookImg from '../../assets/e-book.svg'
+import logoLeadCode from '../../assets/logo1.svg'
+import styles from './styles.module.scss'
+
+const initialState = {
+    user_name: '',
+    from_name: 'leadcoteti@gmail.com',
+    to_name: '',
+    message: `
+        Obrigado por baixar o nosso e-book 'Como criar uma Landing Page que converte.
+        
+       Se esse é o seu primeiro contato com a Leadcode, prazer em conhecê-lo(a)! Se você gostou de nosso exemplo de Landing Page e gostar de nosso conteúdo que acabou de receber, conheça melhor a gente através de nosso site <<siteleadcode.com.br>> e nossos outros projetos.
+        
+        Espero que tenhamos experiências incríveis juntos!
+        `,
+} 
+
+export function FormContact() {
+    const [toSend, setToSend] = useState(initialState);
+
+    const handleChange = (e:any) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e:any) => {
+        e.preventDefault();
+        emailjs.send(
+            'ebook_delivery_service',
+            'ebook_delivery_form',
+            toSend,
+            'uvmZIkRgQPgsOaQKM'
+        )
+        .then((response) => {
+            console.log('SUCCESS', response.status, response.text);
+            alert('O seu e-book foi enviado com sucesso. Confirao o seu e-email.');
+        })
+        .catch((err) => {
+            console.log('FAILED...', err);
+            alert('ERROR')
+        });
+        setToSend(initialState);
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <img src="images/textura-leadcode.svg" />
+            <div className={styles.hr}></div>
+            <h1>Baixe o e-book agora e arrase nas landing pages</h1>
+
+            <div className={styles.contentContainer}>
+                <div className={styles.ebookImg}>
+                    <img src={ebookImg} alt="ebook digital imagem" />
+                </div>
+                <div className={styles.ebookForm}>
+                    <form onSubmit={onSubmit}>
+                        <div className={styles.logo}>
+                            <img src={logoLeadCode} alt="logo LeadCode" />
+                            &lt;LeadCode /&gt;
+                        </div>
+                        <input
+                            type="text" 
+                            name="user_name"
+                            value={toSend.user_name} 
+                            placeholder='Digite o seu nome'
+                            onChange={handleChange}
+                             />
+                        <input 
+                            type="email"
+                            name="to_name" 
+                            value={toSend.to_name} 
+                            placeholder='Digite seu melhor e-mail'
+                            onChange={handleChange} 
+                            />
+                        <button type='submit'>Receber e-book grátis</button>
+                    </form>   
+                </div>
+            
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/testemunhos/index.tsx
@@ -0,0 +1,117 @@
+import styles from './styles.module.scss'
+import boxImg from '../../assets/background-testemunhos.png'
+import imgHenrique from '../../assets/henrique.png'
+import imgLeonardo from '../../assets/leonardo.png'
+import imgJhonny from '../../assets/jhonny.png'
+import imgInes from '../../assets/ines.png'
+import { Swiper, SwiperSlide} from 'swiper/react'
+import { Pagination, A11y } from 'swiper';
+
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+import 'swiper/scss/grid'
+import './slide.scss'
+
+export function Testemunhos() {
+    return (
+        <>
+            <h1 className={styles.testemunhosTitle}>Veja os resultados que produzimos</h1>
+            
+            <div className={styles.container}>         
+                <Swiper
+                    modules={[Pagination, A11y]}
+                    pagination={{ clickable: true }}
+                    spaceBetween={100}
+                    loop
+                    className={styles.swiper}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1,
+                          },
+                          768: {
+                            slidesPerView: 2,
+                          },
+
+                          900: {
+                            slidesPerView: 3,
+                          },
+                      }}
+                    
+                >
+
+                    <SwiperSlide>
+                        <div className={styles.content}>
+                            <p className={styles.apresentation}>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                            </p>     
+
+                            <div className={styles.apresentationImg}>
+                                <img className={styles.boxImg} src={boxImg} alt="background de testemunho" />
+                                <img className={styles.imgTestimony} src={imgHenrique} alt="imagem de testemunho" />
+                            <div className={styles.personInfos}>
+                                <h3>Henrique Pereira</h3>
+                                <p>Desenvolvedor Front-end</p>
+                            </div>                    
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className={styles.content}>
+                            <p className={styles.apresentation}>
+                            A Leadcode mostrou que vai além da técnica em desenvolvimento, ela tem uma preocupação real de que o seu trabalho ajude o cliente a alcançar oss seus objetivos, ou seja, chamar a pessoa que acessa a Landing Page para a ação.  Li o seu ebook e tudo que eles mostram lá, eles aplicam e os resutados que tenho tido são a prova de que dá certo! 🙂 
+                            </p>     
+
+                            <div className={styles.apresentationImg}>
+                                <img className={styles.boxImg} src={boxImg} alt="background de testemunho" />
+                                <img className={styles.imgTestimony} src={imgLeonardo} alt="imagem de testemunho" />
+                            <div className={styles.personInfos}>
+                                <h3>Leonardo Costa</h3>
+                                <p>Desenvolvedor Full-Stack</p>
+                            </div>                    
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className={styles.content}>
+                            <p className={styles.apresentation}>
+                                Realmente o e-book da LeadCode me ajudou bastante a melhorar as habilidades  e técnicas de desenvolvimento de uma landing page. Um e-book simples que  mostra o passo a passo de como criar uma landing page de sucesso e com altas chances de conversão.  Recomendo muito!   
+                            </p>     
+
+                            <div className={styles.apresentationImg}>
+                                <img className={styles.boxImg} src={boxImg} alt="background de testemunho" />
+                                <img className={styles.imgTestimony} src={imgJhonny} alt="imagem de testemunho" />
+                            <div className={styles.personInfos}>
+                                <h3>Jhonny Monteiro</h3>
+                                <p>Desenvolvedor Front-end</p>
+                            </div>                    
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className={styles.content}>
+                            <p className={styles.apresentation}>
+                                Realmente o e-book da LeadCode me ajudou bastante a melhorar as habilidades  e técnicas de desenvolvimento de uma landing page. Um e-book simples que  mostra o passo a passo de como criar uma landing page de sucesso e com altas chances de conversão.  Recomendo muito!   
+                            </p>     
+
+                            <div className={styles.apresentationImg}>
+                                <img className={styles.boxImg} src={boxImg} alt="background de testemunho" />
+                                <img className={styles.imgTestimony} src={imgInes} alt="imagem de testemunho" />
+                            <div className={styles.personInfos}>
+                                <h3>Inês Catarina</h3>
+                                <p>Designer</p>
+                            </div>                    
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    
+                </Swiper>
+                
+               
+            </div>
+            
+        </>
+    )
+}
